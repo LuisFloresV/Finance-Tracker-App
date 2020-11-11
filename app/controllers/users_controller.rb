4d04fb56d4b3e6d920ee212c4770c75c8b74d6ae
Luis Flores V <65628587+LuisFloresV@users.noreply.github.com>
2020-11-10 18:17:40 -0600

    users profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   def my_portfolio
+    @user = current_user
     @tracked_stocks = current_user.stocks
+  end
+  def show
+    @user = User.find(params[:id])
+    @tracked_stocks = @user.stocks
   end
 
   def my_friends
@@ -13,18 +18,18 @@ class UsersController < ApplicationController
       @friends = current_user.except_current_user(@friends)
       if @friends
         respond_to do |format|
-          format.js { render partial: 'users/friend_result'}
+          format.js { render partial: 'users/friend_result' }
         end
       else
         respond_to do |format|
-          flash.now[:alert] = "Could not find user!"
-          format.js { render partial: 'users/friend_result'}
+          flash.now[:alert] = 'Could not find user!'
+          format.js { render partial: 'users/friend_result' }
         end
       end
     else
       respond_to do |format|
-        flash.now[:alert] = "Please enter a friend name or email to search!"
-        format.js { render partial: 'users/friend_result'}
+        flash.now[:alert] ='Please enter a friend name or email to search!'
+        format.js { render partial: 'users/friend_result' }
       end
     end
   end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,6 +2,7 @@ class Stock < ApplicationRecord
   has_many :user_stocks
   has_many :users, through: :user_stocks
   validates :name, :ticker, presence: true
+  
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
       publishable_token: Rails.application.credentials.iex_client[:publishable_token],
@@ -11,14 +12,11 @@ class Stock < ApplicationRecord
     begin 
       new(ticker: ticker_symbol, name: (client.company(ticker_symbol).company_name), last_price: client.price(ticker_symbol), percent: client.quote(ticker_symbol).change_percent_s)
     rescue => exception
-      return nil 
+      return nil
     end
   end
-
   def self.check_db(ticker_symbol)
     where(ticker: ticker_symbol).first
   end
-
-
-
+  
 end
